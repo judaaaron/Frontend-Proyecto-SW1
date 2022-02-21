bdd9f39c1faaec4d014611ce250e4b25a2ef9e94
judaaaron <judaponce14@unitec.edu>
2022-02-20 21:44:04 -0600

    interfaz login

--- Interfaz-drofami/drofami-app/App.js
+++ Interfaz-drofami/drofami-app/App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+// import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg';
+// import ButtonGradient from './components/ButtontGradiente';
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+import DrofamiApp from './app/index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([require('./assets/logoD.jpg'),require('./assets/fondo.jpg')]);
+
+    await Promise.all([...imageAssets]);
+  }
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return <DrofamiApp/>
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +49,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
+
+  //   mainContainer: {
+  //     backgroundColor: '#1292BA',
+  //     flex: 1,
+
+  //   },
+  //   container: {
+
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+
+  //   },
+
+  //   containerImage: {
+  //     flex: 2,
+  //     justifyContent: 'flex-start',
+  //     alignItems: 'center',
+
+  //   },
+
+  //   titulo: {
+  //     fontSize: 35,
+  //     color: '#FAFAFA',
+  //     fontFamily: 'condensed bold',
+  //     fontWeight: 'bold',
+  //   },
+  //   subTitle: {
+  //     fontSize: 20,
+  //     color: '#FAFAFA',
+  //   },
+  //   textInput: {
+  //     padding: 10,
+  //     paddingStart: 21,
+  //     width: '80%',
+  //     height: 45,
+  //     borderColor: 'gray',
+  //     marginTop: 20,
+  //     borderRadius: 30,
+  //     backgroundColor: '#fff',
+  //   }
 });
