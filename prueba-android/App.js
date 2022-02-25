263889d6ce45e438c47ceac47dcaab40d80eeeaf
judaaaron <judaponce14@unitec.edu>
2022-02-24 22:31:32 -0600

    correcciones en archivos

--- prueba-android/App.js
+++ prueba-android/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Colors } from '../components/styles'
-const { primary, tertiary } = Colors;
-import Login from '../screens/Login'
-import Signup from '../screens/Signup'
+//import { Colors } from '../prueba-android/components/styles'
+//const { primary, tertiary } = Colors;
+import Login from './screens/Login'
+import Signup from './screens/Signup'
 //import Home from './screens/Home'
 
 
