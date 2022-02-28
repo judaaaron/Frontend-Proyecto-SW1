1903ca4450dbb474e32824e266818c217b91cecd
judaaaron <judaponce14@unitec.edu>
2022-02-28 15:05:37 -0600

    home de Pauilna y Guillermo actualizado

--- drofami-app/screens/Home.js
+++ drofami-app/screens/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { Text, View } from 'react-native';//agregado
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';//agregado
 import {
     StyledContainer,
     InnerContainer,
@@ -27,35 +31,81 @@ import {
 const Home = () => {
     return (
         <>
-            <StatusBar style="light" />
+            
+            <StyledContainer>
+            <StatusBar style="dark" />
             <InnerContainer>
-                <WelcomeImage
-                    resizeMode="cover"
+                <PageLog
                     source={require("../assets/drofamilogo1.jpg")}
+                    resizeMode="cover"
                 />
-                <WelcomeContainer>
-                    <PageTitle welcome={true} >Drogueria y Farmacia</PageTitle>
-                    <PageTitle welcome={true} >Centroámerica Milenio</PageTitle>
-                   <StyledFormArea>
-                       <Avatar resizeMode="cover" source={require("../assets/azul.png")} />
-                        <StyledButton >
-                            <ButtonText>
-                                Cerrar Sesion
-                            </ButtonText>
-                        </StyledButton>
+                
+                    <PageTitle>Productos</PageTitle>
+                    <Subtitle>Ancalmo</Subtitle>
+                    <Avatar resizeMode="cover" source={require("../assets/azul.png")} />
+                   <Subtitle>Hessel</Subtitle>
+                    <Avatar resizeMode="cover" source={require("../assets/hessel1.png")} />
 
-                    </StyledFormArea>
-                </WelcomeContainer>
+                
             </InnerContainer>
+            </StyledContainer>
         </>
+        /*<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!!</Text>
+        </View>*/
+        
     );
 };
 
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
+const Tab = createBottomTabNavigator();
 
-export default Home;
-
-{
+export default Home;{
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Home') {
+              return (
+                <Ionicons
+                  name={
+                    focused
+                      ? 'ios-information-circle'
+                      : 'ios-information-circle-outline'
+                  }
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'Settings') {
+              return (
+                <Ionicons
+                  name={focused ? 'ios-list-box' : 'ios-list'}
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+          },
+          tabBarInactiveTintColor: 'gray',
+          tabBarActiveTintColor: 'tomato',
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ tabBarBadge: 3 }}
+        />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
     /* <Button onPress={() => setOpenModal(!openModal)}>
               <Text>Holaa</Text>
           </Button>
