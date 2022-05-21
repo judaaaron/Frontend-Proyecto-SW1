e0fe00fffb4218e2a2ae77a45e608637166eb1b7
Andres Nuila <enuila634@gmail.com>
2022-05-21 09:51:49 -0600

    Nueva revision en el redireccioamiento con iOS, esperando que funcione

---
+++ drofami-app/AppDelegate.m
@@ -0,0 +1,16 @@
+#import <React/RCTLinkingManager.h>
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
