a969af83424817ea2f1b2126073ad9debc566176
Paulina Euceda <104641512+PaulinaEuceda@users.noreply.github.com>
2022-06-11 20:28:09 -0600

    Navigation en recomendaciones completado

--- drofami-app/screens/MainHome.js
+++ drofami-app/screens/MainHome.js
@@ -16,6 +16,10 @@ import {
 import { data } from './OfertasAncalmo'
 import { useSelector } from "react-redux";//este se agrega
 import { useIsFocused } from "@react-navigation/native";
+import Carousel, { Pagination } from "react-native-snap-carousel";
+import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from "./CarouselCardItem";
+import { getProduct } from "../src/ProductMethods";
+import { showMessage } from "react-native-flash-message";
 
 
 export default function MainHome({ navigation }) {
@@ -26,6 +30,10 @@ export default function MainHome({ navigation }) {
   const [productResponse, setProductResponse] = useState();
   const [recomendacionANC, setRecomendacionANC] = useState([]);
   const [recomendacionHES, setRecomendacionHES] = useState([]);
+  const [index, setIndex] = React.useState(0);
+  const isCarousel = React.useRef(null);
+  const isEmpleado = React.useRef(useSelector((state) => state.staff.value));
+  // console.log("fish ", recomendacionANC[0]['id']);
 
 
   const isFocused = useIsFocused();
@@ -52,58 +60,205 @@ React.useEffect(() => {
       return;
   }
   setRecomendacionHES((response['data']['HES']))
-  console.log(response)
+  // console.log(response)
 }, [response]);
 
 React.useEffect(() => {
   if (!recomendacionANC) {
       return;
   }
-  console.log(recomendacionANC)
+  // console.log(recomendacionANC)
 }, [recomendacionANC]);
 
 React.useEffect(() => {
   if (!recomendacionHES) {
       return;
   }
-  console.log(recomendacionHES)
+  // console.log(recomendacionHES)
 }, [recomendacionHES]);
 
+
+React.useEffect(() => {
+  if (!productResponse) {
+    return;
+  }
+  if (!productResponse["status"]) {
+    showMessage({
+      message: "Ha ocurrido un error inesperado.",
+      description: "Intente más tarde",
+      type: "danger",
+    });
+    // alert('Ocurrió un error inesperado')
+    return;
+  }
+  if (productResponse["status"] == "failed") {
+    showMessage({
+      message: productResponse["message"],
+      // description: "Failed",
+      type: "danger",
+    });
+    // alert(productResponse['message']);
+  }
+  const product = productResponse["data"];
+
+  if (isEmpleado.current) {
+    navigation.navigate("EmpleadoDetalleProductoScreen", {
+      id: product["producto"]["id"],
+      cantidad: product["cantidad"],
+      imagen: product["producto"]["imagen"],
+      nombre: product["producto"]["nombre"],
+      precio: product["producto"]["precio"],
+      fabricante: product["producto"]["fabricante"],
+      color: product["producto"]["color"],
+    });
+  } else {
+    navigation.navigate("DetalleProductsAncalmo", {
+      id: product["producto"]["id"],
+      cantidad: product["cantidad"],
+      imagen: product["producto"]["imagen"],
+      nombre: product["producto"]["nombre"],
+      precio: product["producto"]["precio"],
+      fabricante: product["producto"]["fabricante"],
+      indicaciones: product["producto"]["indicaciones"],
+      dosis: product["producto"]["dosis"],
+      formula: product["producto"]["formula"],
+      color: product["producto"]["color"],
+    });
+  }
+}, [productResponse]);
+
   return (
-
-
     <View style={styles.container}>
       <PageLogOferta
         source={require("../assets/drofamilogo1.jpg")}
         resizeMode="cover"
       />
-      <Text style={{ marginLeft: 20, marginTop: 30, top: -20, fontWeight: 'bold', color: Colors.blue, fontSize: 20 }}>Productos recomendados para tí</Text>
+      <Text
+        style={{
+          marginLeft: 20,
+          marginTop: 30,
+          top: -20,
+          fontWeight: "bold",
+          color: Colors.blue,
+          fontSize: 20,
+        }}
+      >
+        Productos recomendados para tí
+      </Text>
       <ScrollView
         // scrollIndicatorInsets={false}
         showsVerticalScrollIndicator={false}
       >
-        <Image source={require("./../assets/logoAncalmo.png")}
-          style={{ width: 100, height: 100, top: 3, backgroundColor: Colors.primary, marginLeft: 125 }}
+        <Image
+          source={require("./../assets/logoAncalmo.png")}
+          style={{
+            width: 100,
+            height: 100,
+            top: 3,
+            backgroundColor: Colors.primary,
+            marginLeft: 125,
+          }}
         />
         <StyledContainer style={{ marginTop: 10 }}>
-          <TouchableOpacity onPress={() => console.log('producto')} style={styles.viiew}>
-
-            <CarouselCards data={recomendacionANC} />
-
+          <TouchableOpacity
+            onPress={() => console.log("producto")}
+            style={styles.viiew}
+          >
+            {/* <CarouselCards data={recomendacionANC} /> */}
+            <Pressable
+              onPress={() => {getProduct(setLoading, token.current,  recomendacionANC[index]['id'], setProductResponse)}}
+            > 
+              <View>
+                <Carousel
+                  // layout="stack"
+                  layoutCardOffset={19}
+                  ref={isCarousel}
+                  data={recomendacionANC}
+                  renderItem={CarouselCardItem}
+                  sliderWidth={SLIDER_WIDTH}
+                  itemWidth={ITEM_WIDTH}
+                  onSnapToItem={(index) => setIndex(index)}
+                  useScrollView={true}
+                  autoplay={true}
+                  loop={true}
+                />
+                <Pagination
+                  dotsLength={recomendacionANC.length}
+                  activeDotIndex={index}
+                  carouselRef={isCarousel}
+                  dotStyle={{
+                    width: 10,
+                    height: 1,
+                    borderRadius: 5,
+                    marginHorizontal: 0,
+                    backgroundColor: "rgba(0, 0, 0, 0.92)",
+                  }}
+                  inactiveDotOpacity={0.4}
+                  inactiveDotScale={0.6}
+                  tappableDots={true}
+                  autoplay={true}
+                  loop={true}
+                />
+              </View>
+            </Pressable>
           </TouchableOpacity>
         </StyledContainer>
 
-        <Image source={require("./../assets/hesselLogo1.png")}
-          style={{ width: 100, height: 100, top: 3, backgroundColor: Colors.primary, marginLeft: 125, marginTop: -60 }}
+        <Image
+          source={require("./../assets/hesselLogo1.png")}
+          style={{
+            width: 100,
+            height: 100,
+            top: 3,
+            backgroundColor: Colors.primary,
+            marginLeft: 125,
+            marginTop: -60,
+          }}
         />
         <StyledContainer style={{ marginTop: -2 }}>
           <TouchableOpacity style={styles.viiew}>
-            <CarouselCards2  data={recomendacionHES}/>
+            {/* <CarouselCards2 data={recomendacionHES} /> */}
+
+            <Pressable
+              onPress={() => {getProduct(setLoading, token.current,  recomendacionHES[index]['id'], setProductResponse)}}
+            > 
+              <View>
+                <Carousel
+                  // layout="stack"
+                  layoutCardOffset={19}
+                  ref={isCarousel}
+                  data={recomendacionHES}
+                  renderItem={CarouselCardItem}
+                  sliderWidth={SLIDER_WIDTH}
+                  itemWidth={ITEM_WIDTH}
+                  onSnapToItem={(index) => setIndex(index)}
+                  useScrollView={true}
+                  autoplay={true}
+                  loop={true}
+                />
+                <Pagination
+                  dotsLength={recomendacionHES.length}
+                  activeDotIndex={index}
+                  carouselRef={isCarousel}
+                  dotStyle={{
+                    width: 10,
+                    height: 1,
+                    borderRadius: 5,
+                    marginHorizontal: 0,
+                    backgroundColor: 'rgba(0, 0, 0, 0.92)'
+                  }}
+                  inactiveDotOpacity={0.4}
+                  inactiveDotScale={0.6}
+                  tappableDots={true}
+                  autoplay={true}
+                  loop={true}
+                />
+              </View>
+            </Pressable>
           </TouchableOpacity>
         </StyledContainer>
       </ScrollView>
     </View>
-
   );
 }
 
